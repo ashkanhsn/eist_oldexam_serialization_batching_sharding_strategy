ff571e1c90c69b9b647d9421fdec51b87a0a9f0c
Ashkan-ash <ashkan1995.as@gmail.com>
2026-05-22 21:05:24 +0200

    TODO 2.1

--- src/miningshaft/ManualReplicationStrategy.java
+++ src/miningshaft/ManualReplicationStrategy.java
@@ -7,10 +7,20 @@ public class ManualReplicationStrategy implements ReplicationStrategy {
     @Override
     public void writeToRemotes(Gem g, ArrayList<Replica> replicas) {
         // TODO Q 2.1
+        for (Replica r : replicas){
+            if (!r.isBroken()){
+                r.sendGem(g.id, g);
+            }
+        }
     }
 
     @Override
     public void writeBatchToRemotes(HashMap<Integer, Gem> g, ArrayList<Replica> replicas) {
         // TODO Q 2.1
+        for (Replica r:replicas){
+            if (!r.isBroken()){
+                r.sendBatch(g);
+            }
+        }
     }
 }
